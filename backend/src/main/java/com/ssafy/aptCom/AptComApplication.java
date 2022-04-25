6c4a0e6c648dc6671f2ed2c88271ae8be421c951
dudqo225 <dudqo225@gmail.com>
2022-04-25 09:48:55 +0900

    Chore : 프로젝트 초기 셋팅

---
+++ backend/src/main/java/com/ssafy/aptCom/AptComApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.aptCom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AptComApplication.class, args);
+	}
+
+}
