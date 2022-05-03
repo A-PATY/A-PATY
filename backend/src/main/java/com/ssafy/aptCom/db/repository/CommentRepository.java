b79828527d0634e245c94e42c1df8f9cf4627bbc
escho <koren2696@naver.com>
2022-05-03 10:42:59 +0900

    Feat : 게시글 수정, 삭제

---
+++ backend/src/main/java/com/ssafy/aptCom/db/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+package com.ssafy.aptCom.db.repository;
+
+import com.ssafy.aptCom.db.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
+
+}
