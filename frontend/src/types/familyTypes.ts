ff5403ba49ceeeb920d33f5827e2ff14dc288dbe
김선민 <smkim0903@naver.com>
2022-05-04 12:06:13 +0900

    Feat : 가족 위치 찾기 기본 기능 구현

--- frontend/src/types/familyTypes.ts
+++ frontend/src/types/familyTypes.ts
@@ -1,12 +1,21 @@
-// export interface familyList {
-//   familyList: member[];
-// }
-
 export interface memberProps {
   member: {
     userId: number;
     userName: string;
     findFamily: boolean; 
-    userProfileImgUrl: string;
-  }
+    profileImgUrl: string;
+  },
+  changeMember: (member: familyList) => void, // 아무것도 리턴하지 X
+};
+
+export interface familyList {
+  userId: number;
+  userName: string;
+  findFamily: boolean; 
+  profileImgUrl: string;
+};
+
+export interface location {
+  lat: number;
+  lng: number;
 }
