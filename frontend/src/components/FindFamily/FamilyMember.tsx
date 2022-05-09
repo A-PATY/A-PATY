250ab0054320023bd20c25371251067f2191a54a
김선민 <smkim0903@naver.com>
2022-05-09 14:48:59 +0900

    Feat : 위치찾기 허용 여부 구현

--- frontend/src/components/FindFamily/FamilyMember.tsx
+++ frontend/src/components/FindFamily/FamilyMember.tsx
@@ -1,19 +1,65 @@
 import { useState } from 'react';
 import styled from '@emotion/styled';
 import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
 import { memberProps } from '../../types/familyTypes';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import LocationOffIcon from '@mui/icons-material/LocationOff';
 
 const FamilyMember: React.FC<memberProps> = ({ member, changeMember }) => {
-  
   return (
     <>
       <Container>
+        {/* <AvatarCustom 
+          src={member.profileImgUrl} 
+          alt={member.userName} 
+          onClick={() => {changeMember(member)}}
+          style={{ cursor: member.findFamily ? "pointer" : "default" }}
+        />
+        <MemberInfo 
+          onClick={() => {changeMember(member)}} 
+          style={{ cursor: member.findFamily ? "pointer" : "default" }}
+        >
+          <MemberName>{member.userName}</MemberName>
+          {
+            member.findFamily ?
+            <GpsClassify>위치찾기: 허용 <LocationOnIcon/></GpsClassify> :
+            <GpsClassify>위치찾기: 미허용</GpsClassify> 
+          }
+        </MemberInfo> */}
+
         <AvatarCustom 
           src={member.profileImgUrl} 
           alt={member.userName} 
           onClick={() => {changeMember(member)}}
+          style={{ cursor: member.findFamily ? "pointer" : "default" }}
         />
-        <AuthorName onClick={() => {changeMember(member)}}>{member.userName}</AuthorName>
+        <MemberInfo 
+          onClick={() => {changeMember(member)}} 
+          style={{ cursor: member.findFamily ? "pointer" : "default" }}
+        >
+          <MemberName>{member.userName}</MemberName>
+          {
+            member.findFamily ?
+            <LocationOnIcon/> :
+            <Tooltip 
+              title="위치찾기 미허용" 
+              placement="top"
+              componentsProps={{ 
+                tooltip: { 
+                  sx: {
+                    backgroundColor: "#eae7e7",
+                    color: 'rgba(0, 0, 0, 0.87)',
+                    fontSize: 11,
+                    padding: "5px 6px",
+                  }
+                }
+              }}
+            >
+              <LocationOffIcon/>
+            </Tooltip>
+          }
+        </MemberInfo>
       </Container>
     </>
   );
@@ -28,19 +74,37 @@ const Container = styled.div`
 const AvatarCustom = styled(Avatar)`
   width: 55px;
   height: 55px;
-  cursor: pointer;
 `;
 
-const AuthorName = styled.h3`
+const MemberName = styled.h3`
   max-width: 120px;
-  margin-left: 20px;
   height: 20px;
   line-height: 20px;
   font-size: 16px;
   font-weight: bold;
   color: #292929;
   white-space: nowrap;
-  cursor: pointer;
+`;
+
+// const MemberInfo = styled.div`
+//   margin-left: 20px;
+// `;
+
+const MemberInfo = styled.div`
+  margin-left: 20px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  & .MuiSvgIcon-root {
+    font-size: 20px;
+    color: #9e9d9d;
+    margin-left: 5px;
+  }
+`;
+
+const GpsClassify = styled.p`
+  margin-top: 3px;
+  font-size: 13px;
 `;
 
 export default FamilyMember;
