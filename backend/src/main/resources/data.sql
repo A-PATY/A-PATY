feff359aeafbd939b587267494ab31ab734d7437
dudqo225 <dudqo225@gmail.com>
2022-05-11 10:53:49 +0900

    Modify : nickName > nickname 일괄 변경

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -8,7 +8,7 @@ INSERT INTO APARTMENT(id, apt_code, apt_name) VALUES(1, '1', '장미아파트');
 
 --INSERT INTO USER_COMMUNITY(id, community_id, user_id) VALUES(1, 1, 1);
 
-INSERT INTO USER(id, nickname) VALUES(1, '장미');
+--INSERT INTO USER(id, nickname) VALUES(1, '장미');
 
 INSERT INTO BASE_ADDRESS(id, address, dong_name, gugun_name, ri_name, sido_name) VALUES(1, '111', '장미동', '장미구', '짱미리', '장미시');
 
