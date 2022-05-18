3da8ef289838e88855c862e31b3e13d338fa7717
YeEun <codpdms@naver.com>
2022-05-18 16:50:04 +0900

    Modify: 글 목록에서 작성시간 표시 수정

--- frontend/src/components/Community/Local/BoardArticle.tsx
+++ frontend/src/components/Community/Local/BoardArticle.tsx
@@ -40,6 +40,31 @@ const BoardArticle: React.FC<Props> = ({ article }) => {
     navigate(`/board/${article.articleId}`);
   };
 
+  const calculateTime = (time: string) => {
+    const today = new Date();
+    const timeValue = new Date(time);
+    const betweenTime = Math.floor(
+      (today.getTime() - timeValue.getTime()) / 1000 / 60,
+    );
+
+    if (betweenTime < 1) return '방금전';
+    if (betweenTime < 60) {
+      return `${betweenTime}분전`;
+    }
+
+    const betweenTimeHour = Math.floor(betweenTime / 60);
+    if (betweenTimeHour < 24) {
+      return `${betweenTimeHour}시간전`;
+    }
+
+    const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
+    if (betweenTimeDay < 365) {
+      return `${betweenTimeDay}일전`;
+    }
+
+    return time;
+  };
+
   return (
     <ArticleWrapper key={article.articleId}>
       <Category>
@@ -64,7 +89,7 @@ const BoardArticle: React.FC<Props> = ({ article }) => {
       </Article>
       <ArticleInfoWrapper>
         <ArticleInfo>
-          <Info>{article.createdAt}</Info>
+          <Info>{calculateTime(article.createdAt)}</Info>
           <Info>{article.author}</Info>
         </ArticleInfo>
         <ArticleInfo>
